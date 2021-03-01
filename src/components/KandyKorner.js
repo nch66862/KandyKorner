cfcae8a2a0fe7dbdb428f8a2d613bd345ba41bd4
Nick Carver <nicholascarver2@outlook.com>
2021-03-01 14:47:01 -0600

    set up the nav bar

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom"
 import { LocationList } from "./locations/LocationList"
 import { LocationProvider } from "./locations/LocationProvider"
+import { NavBar } from "./nav/NavBar"
 
 export const KandyKorner = () => {
     return (
         <>
             <h1>This is Kandy Korner</h1>
+            <Router>
+                <NavBar />
+            </Router>
             <LocationProvider>
                 <LocationList />
             </LocationProvider>
